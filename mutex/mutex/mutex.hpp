fbdf5cc9701221bf9b666f9897fb2e3a3a8a9bbe
Максим Егорович Кокряшкин <kokriashkin.me@phystech.edu>
2021-03-13 23:36:36 +0000

    [mipt] [Maxim-Kokryashkin] mutex/mutex

---
+++ mutex/mutex/mutex.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <bits/stdint-intn.h>
+#include <twist/stdlike/atomic.hpp>
+#include <twist/util/spin_wait.hpp>
+
+#include <cstdlib>
+
+namespace solutions {
+
+using twist::util::SpinWait;
+
+class Mutex {
+ public:
+  void Lock() {
+    while (locked_.exchange(1) != 0) {
+      n_threads_.fetch_add(1);
+      locked_.wait(1);
+      n_threads_.fetch_sub(1);
+    }
+  }
+
+  void Unlock() {
+    locked_.exchange(0);
+
+    if (n_threads_.load() != 0u) {
+      locked_.notify_one();
+    }
+  }
+
+ private:
+  twist::stdlike::atomic<unsigned int> locked_;
+  twist::stdlike::atomic<unsigned int> n_threads_{0};
+};
+
+}  // namespace solutions
