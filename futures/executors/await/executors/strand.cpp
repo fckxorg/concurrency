bee414c961e18d046cc4077ffbe02f25b1d7ab13
Maxim Kokryashkin <>
2021-04-14 14:00:26 +0300

    priority with guarded

--- futures/executors/await/executors/strand.cpp
+++ futures/executors/await/executors/strand.cpp
@@ -1,9 +1,33 @@
 #include <await/executors/strand.hpp>
 #include <await/executors/helpers.hpp>
+#include <await/executors/guarded.hpp>
+#include <queue>
 
 namespace await::executors {
 
-IExecutorPtr MakeStrand(IExecutorPtr /*executor*/) {
+class Strand : public IExecutor {
+ private:
+  Guarded<std::queue<Task>> tasks_;
+  IExecutorPtr executor_;
+
+ public:
+  Strand(IExecutorPtr executor) : executor_(executor) {
+  }
+
+  void Execute(Task&& task) {
+    tasks_.push(std::move(task));
+
+    executor_->Execute([this]() {
+      Task routine = std::move(tasks_->front());
+      tasks_->pop();
+      routine();
+    });
+  }
+
+}
+
+IExecutorPtr
+MakeStrand(IExecutorPtr /*executor*/) {
   return nullptr;  // Not implemented
 }
 
